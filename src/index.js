c2b260c04a6f3728419c877ef1b24421c56596e4
Aditya Porwal <adityaporwal3251@gmail.com>
2026-01-17 09:53:03 +0530

    custom response

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
+import express from 'express'
 import connectDB from './db/index.js'
 dotenv.config({
     path:'./env'
 })
+const app = express()
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT||8000,()=>{
+        console.log(`process is running on port : ${process.env.PORT}`)
+    })
+})
+.catch((error)=>{
+    console.log(`error in connecting connectDB`,error)
+})
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,18 @@
+// const asyncHandler = (func)=>async(res,req,next)=>{
+//     try {
+//         await func(res,req,next)
+//     } catch (error) {
+//         res.status(error.code||400)
+//         .json({
+//             success:false,
+//             message:error.message
+//         })
+//     }
+
+// }
+const asyncHandler = (requestHandler)=>{
+    (req,res,next)=>{
+        Promise.resolve(requestHandler(req,res,next)).catch((error)=>next(error))
+    }
+}
+export {asyncHandler}
