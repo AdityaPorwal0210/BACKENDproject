0853ecface171b676926731747ec61c2f75d3be2
Aditya Porwal <adityaporwal3251@gmail.com>
2026-01-18 12:28:05 +0530

    created routes ,then used post,and tested route is working with postman

--- src/app.js
+++ src/app.js
@@ -11,5 +11,10 @@ app.use(express.json({limit:"16Kb"}))
 app.use(express.static('public'))
 app.use(express.urlencoded({extended:true,limit:"16Kb"}))
 app.use(cookieParser())
-export default app
+
+//routes=>
+import router from "./routes/user.routes.js"
+app.use('/api/v1/users',router)
+
+export {app}
 
--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 import express from 'express'
 import connectDB from './db/index.js'
+import {app} from './app.js'
 dotenv.config({
     path:'./env'
 })
-const app = express()
 connectDB()
 .then(()=>{
     app.listen(process.env.PORT||8000,()=>{
